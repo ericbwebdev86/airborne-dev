51f564c23dbb07bcca6543ac65cfb326e6bbd189
unknown <ericbwebdev86@gmail.com>
2022-04-15 10:13:03 -0500

    added link stylings for active links

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -8,16 +8,16 @@ return(
         <p className="text-center nav-link text-light display-6" >Eric Bates</p>
         <ul className="nav nav-fill justify-content-center text-light">
           <li className="nav-item">
-            <a href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active lead' : 'nav-link lead'}>About</a>
+            <a href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active text-success text-decoration-underline fw-bold lead' : 'nav-link lead text-light'}>About</a>
           </li>
           <li className="nav-item">
-            <a  href="#Portfolio" onClick={() => handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'nav-link active lead' : 'nav-link lead'}>Portfolio</a>
+            <a  href="#Portfolio" onClick={() => handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'nav-link active lead text-success text-decoration-underline fw-bold' : 'nav-link lead text-light'}>Portfolio</a>
           </li>
           <li className="nav-item">
-            <a href="#Contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active lead' : 'nav-link lead'}>Contact</a>
+            <a href="#Contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active lead text-success text-decoration-underline fw-bold' : 'nav-link lead text-light'}>Contact</a>
           </li>
           <li className="nav-item">
-            <a href="#Resume" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active lead' : 'nav-link lead'}>Resume</a>
+            <a href="#Resume" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active lead text-success text-decoration-underline fw-bold' : 'nav-link lead text-light'}>Resume</a>
           </li>
         </ul>
       </div>
