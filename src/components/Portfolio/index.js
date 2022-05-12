7755b8bcc77bbd6e8968f3838404c289136d2cde
unknown <ericbwebdev86@gmail.com>
2022-05-11 20:04:28 -0500

    added my-outdoor-space to portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -10,6 +10,17 @@ export default function Portfolio() {
             <div className="border border-dark border-2 rounded-3 container bg-dark text-dark">
                 <h2 className="text-center text-light display-4 mt-2 mb-5">My Portfolio</h2>
                 <div className="row">
+                <div className="col">
+                        <div className="card mt-2 mb-2 card-width" >
+                        <img src="./assets/images/my-outdoor-space.JPG" className="card-img-top" alt="screenshot of memo-saver"/>
+                        <div className="card-body">
+                          <h5 className="card-title">My Outdoor Space</h5>
+                          <p className="card-text">MERN Stack, JWT, Bootstrap, GraphQL, ApolloServer, Concurrently</p>
+                          <a href="https://github.com/p-fassbender/my-outdoor-space" className="btn btn-success me-2">Github Repo</a>
+                              <a href="https://my-outdoor-space.herokuapp.com/" className="btn btn-success">Deployed App</a>
+                        </div>
+                      </div>  
+                    </div>
                     <div className="col">
                         <div className="card mt-2 mb-2 card-width" >
                             <img src="./assets/images/3-guys.JPG" className="card-img-top" alt="screenshot of 3-guys-pizza-n-pies website"/>
@@ -24,7 +35,7 @@ export default function Portfolio() {
                     </div>
                     <div className="col">
                         <div className="card mt-2 mb-2 card-width" >
-                        <img src="./assets/images/erics-tech-talk.JPG" className="card-img-top" alt="screeshot of erics-tech-talk"/>
+                        <img src="./assets/images/erics-tech-talk.JPG" className="card-img-top" alt="screenshot of erics-tech-talk"/>
                         <div className="card-body">
                           <h5 className="card-title">erics-tech-talk</h5>
                           <p className="card-text">This is a fullstack blog site: built with mysql, handlebars, express, and node.</p>
@@ -33,22 +44,12 @@ export default function Portfolio() {
                         </div>
                       </div>  
                     </div>
-                    <div className="col">
-                        <div className="card mt-2 mb-2 card-width" >
-                        <img src="./assets/images/code-quiz.JPG" className="card-img-top" alt="screeshot of code-quiz"/>
-                        <div className="card-body">
-                          <h5 className="card-title">code-quiz</h5>
-                          <p className="card-text">Front-end project. Uses HTML, CSS, and JavaScript, specifically dom manipulation and local storage.</p>
-                          <a href="https://github.com/ericbwebdev86/code-quiz" className="btn btn-success me-2">Github Repo</a>
-                              <a href="https://ericbwebdev86.github.io/code-quiz/" className="btn btn-success">Deployed App</a>
-                        </div>
-                      </div>  
-                    </div>
+                    
                 </div>
                 <div className="row">
                     <div className="col">
                         <div className="card mt-2 mb-2 card-width" >
-                            <img src="./assets/images/work-day-scheduler.JPG" className="card-img-top" alt="screeshot of work-day-scheduler"/>
+                            <img src="./assets/images/work-day-scheduler.JPG" className="card-img-top" alt="screenshot of work-day-scheduler"/>
                             <div className="card-body">
                               <h5 className="card-title">work-day-scheduler</h5>
                               <p className="card-text">Front-end project. Uses HTML, CSS, JavaScript, Bootstrap, jQuery, Momnentjs, and FontAwesome.</p>
@@ -59,7 +60,7 @@ export default function Portfolio() {
                     </div>
                     <div className="col">
                         <div className="card mt-2 mb-2 card-width" >
-                        <img src="./assets/images/elegant-art.JPG" className="card-img-top" alt="screeshot of elegant-art"/>
+                        <img src="./assets/images/elegant-art.JPG" className="card-img-top" alt="screenshot of elegant-art"/>
                         <div className="card-body">
                           <h5 className="card-title">elegant-art</h5>
                           <p className="card-text">Front-end project. This a website made for family. HTML, CSS, JavaScript, Bootstrap. Unfinished.</p>
@@ -70,12 +71,12 @@ export default function Portfolio() {
                     </div>
                     <div className="col">
                         <div className="card mt-2 mb-2 card-width" >
-                        <img src="./assets/images/note-taker.JPG" className="card-img-top" alt="screeshot of memo-saver"/>
+                        <img src="./assets/images/code-quiz.JPG" className="card-img-top" alt="screenshot of code-quiz"/>
                         <div className="card-body">
-                          <h5 className="card-title">memo-saver</h5>
-                          <p className="card-text">Front-end project. Made with HTML, CSS, JavaScript, and ExpressJS.</p>
-                          <a href="https://github.com/ericbwebdev86/memo-saver" className="btn btn-success me-2">Github Repo</a>
-                              <a href="https://github.com/ericbwebdev86/memo-saver" className="btn btn-success">Deployed App</a>
+                          <h5 className="card-title">code-quiz</h5>
+                          <p className="card-text">Front-end project. Uses HTML, CSS, and JavaScript, specifically dom manipulation and local storage.</p>
+                          <a href="https://github.com/ericbwebdev86/code-quiz" className="btn btn-success me-2">Github Repo</a>
+                              <a href="https://ericbwebdev86.github.io/code-quiz/" className="btn btn-success">Deployed App</a>
                         </div>
                       </div>  
                     </div>
