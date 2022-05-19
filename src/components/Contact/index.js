b1ae9ee24066271a96c939291b7f85035f17a6b3
unknown <ericbwebdev86@gmail.com>
2022-05-19 12:23:26 -0500

    added email send functionality to contact form using emailjs

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import { validateEmail } from '../../utils/helpers'
+import emailjs from '@emailjs/browser';
 
 
 export default function Contact() {
@@ -7,6 +8,7 @@ export default function Contact() {
     const [formState, setFormState] = useState({ name: '', email: '', message: ''});
     const [errorMessage, setErrorMessage ] = useState('');
     const { name, email, message} = formState;
+    const form = useRef();
 function handleChange(e) {
     if(e.target.name === 'email') {
         const isValid = validateEmail(e.target.value);
@@ -34,7 +36,15 @@ function handleChange(e) {
 function handleSubmit(e) {
     e.preventDefault();
     if (!errorMessage) {
-        console.log('Form Submitted', formState);
+        // console.log('Form Submitted', formState);
+        
+        emailjs.sendForm('service_0bzdv9e', 'template_vzoh4te', form.current, 'TjV7uoOw11YalcFEN')
+      .then((result) => {
+        window.location.reload(false)
+          alert('Email sent successfully!');
+      }, (error) => {
+          console.log(error.text);
+      });
     }
 };
     return(
@@ -42,7 +52,7 @@ function handleSubmit(e) {
     <div className="about-container container"> 
         <div className="border border-dark border-2 rounded-3 container bg-dark text-light col-12 col-lg-6">
         <h2 className="mt-2 text-center">Contact Me</h2>
-            <form className="mx-5 mb-5 text-start" onSubmit={handleSubmit}>
+            <form className="mx-5 mb-5 text-start" ref={form} onSubmit={handleSubmit}>
                 
                 <div className="mb-3 ">
                     <label htmlFor="name" className="form-label text-start">Your Name</label>
