8623bb7b7ef1156de8d55cf431f4d6ece5e19d97
unknown <ericbwebdev86@gmail.com>
2022-06-06 10:15:36 -0500

    removed malfunctioning icons, changed some stylings, added some project text to the portfolio page

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,7 +5,7 @@ export default function Nav({ currentPage, handlePageChange }) {
 return(
         <ul className="nav nav-fill justify-content-center text-light">
           <li className="nav-item">
-            <a href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active text-success text-decoration-underline fw-bold lead' : 'nav-link lead text-light'}>About</a>
+            <a href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active active-hover text-success text-decoration-underline fw-bold lead' : 'nav-link lead text-light'}>About</a>
           </li>
           <li className="nav-item">
             <a  href="#Portfolio" onClick={() => handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'nav-link active lead text-success text-decoration-underline fw-bold' : 'nav-link lead text-light'}>Portfolio</a>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -16,10 +16,9 @@ export default function Portfolio() {
                         <div className="card-body">
                           <h5 className="card-title">My Outdoor Space</h5>
                           <p className="card-text">MERN Stack group project, forum site JWT, Bootstrap, GraphQL, ApolloServer, Concurrently</p>
-                          <a href="https://github.com/p-fassbender/my-outdoor-space" className="btn btn-success me-2"><i class="fa-brands fa-github"></i>
+                          <a href="https://github.com/p-fassbender/my-outdoor-space" className="btn btn-success me-2 mb-2">
                             <p>Github Repo</p></a>
-                              <a href="https://my-outdoor-space.herokuapp.com/" className="btn btn-success">
-                              <i class="fa-solid fa-sitemap"></i>
+                              <a href="https://my-outdoor-space.herokuapp.com/" className="btn btn-success mb-2 me-2">
                               <p>
                               Deployed App
                               </p></a>
@@ -31,10 +30,10 @@ export default function Portfolio() {
                             <img src="./assets/images/3-guys.JPG" className="card-img-top" alt="screenshot of 3-guys-pizza-n-pies website"/>
                             <div className="card-body">
                               <h5 className="card-title">3-guys-pizza-n-pies</h5>
-                              <p className="card-text">Fullstack group project: eCommerce site built with mysql, handlebars, express, and node.</p>
-                              <a href="https://github.com/ericbwebdev86/3-guys-pizza-n-pies" className="btn me-2 btn-success"><i class="fa-brands fa-github"></i>
+                              <p className="card-text">Fullstack group project: eCommerce site built with SQL/Sequelize, handlebars, express, bcrypt and node.</p>
+                              <a href="https://github.com/ericbwebdev86/3-guys-pizza-n-pies" className="btn me-2 btn-success mb-2">
                             <p>Github Repo</p></a>
-                              <a href="https://agile-bastion-05286.herokuapp.com/" className="btn btn-success"><i class="fa-solid fa-sitemap"></i>
+                              <a href="https://agile-bastion-05286.herokuapp.com/" className="btn btn-success mb-2 me-2">
                               <p>
                               Deployed App
                               </p></a>
@@ -42,33 +41,50 @@ export default function Portfolio() {
     </div>
                           
                     </div>
+                    {/* below */}
                     <div className="col">
                         <div className="card mt-2 mb-2" >
-                        <img src="./assets/images/erics-tech-talk.JPG" className="card-img-top" alt="screenshot of erics-tech-talk"/>
+                        <img src="./assets/images/elegant-art.JPG" className="card-img-top" alt="screenshot of elegant-art"/>
                         <div className="card-body">
-                          <h5 className="card-title">erics-tech-talk</h5>
-                          <p className="card-text">This is a fullstack blog site: built with mysql, handlebars, express, and node.</p>
-                          <a href="https://github.com/ericbwebdev86/erics-tech-talk" className="btn btn-success me-2"><i class="fa-brands fa-github"></i>
+                          <h5 className="card-title">elegant-art</h5>
+                          <p className="card-text">Front-end project. This a website made for a relative's stained glass business. HTML, CSS, JavaScript, Bootstrap. Unfinished.</p>
+                          <a href="https://github.com/ericbwebdev86/elegant-art" className="btn btn-success me-2 mb-2">
                             <p>Github Repo</p></a>
-                              <a href="https://enigmatic-tor-99381.herokuapp.com/" className="btn btn-success"><i class="fa-solid fa-sitemap"></i>
+                              <a href="http://elegantarte.com/" className="btn btn-success mb-2 me-2">
                               <p>
                               Deployed App
                               </p></a>
                         </div>
                       </div>  
                     </div>
-                    
+                    {/* above */}
                 </div>
                 <div className="row">
                     <div className="col">
-                        <div className="card mt-2 mb-2" >
+                        
+                          <div className="card mt-2 mb-2" >
+                        <img src="./assets/images/erics-tech-talk.JPG" className="card-img-top" alt="screenshot of erics-tech-talk"/>
+                        <div className="card-body">
+                          <h5 className="card-title">erics-tech-talk</h5>
+                          <p className="card-text">This is a fullstack blog site: built with SQL/Sequelize, handlebars, express, and node.</p>
+                          <a href="https://github.com/ericbwebdev86/erics-tech-talk" className="btn btn-success me-2 mb-2">
+                            <p>Github Repo</p></a>
+                              <a href="https://enigmatic-tor-99381.herokuapp.com/" className="btn btn-success me-2 mb-2">
+                              <p>
+                              Deployed App
+                              </p></a>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col">
+                    <div className="card mt-2 mb-2" >
                             <img src="./assets/images/work-day-scheduler.JPG" className="card-img-top" alt="screenshot of work-day-scheduler"/>
                             <div className="card-body">
                               <h5 className="card-title">work-day-scheduler</h5>
                               <p className="card-text">Front-end project. Uses HTML, CSS, JavaScript, Bootstrap, jQuery, Momnentjs, and FontAwesome.</p>
-                              <a href="https://github.com/ericbwebdev86/work-day-scheduler" className="btn btn-success me-2"><i class="fa-brands fa-github"></i>
+                              <a href="https://github.com/ericbwebdev86/work-day-scheduler" className="btn btn-success me-2 mb-2">
                             <p>Github Repo</p></a>
-                              <a href="https://ericbwebdev86.github.io/work-day-scheduler/" className="btn btn-success"><i class="fa-solid fa-sitemap"></i>
+                              <a href="https://ericbwebdev86.github.io/work-day-scheduler/" className="btn btn-success me-2 mb-2">
                               <p>
                               Deployed App
                               </p></a>
@@ -77,28 +93,13 @@ export default function Portfolio() {
                     </div>
                     <div className="col">
                         <div className="card mt-2 mb-2" >
-                        <img src="./assets/images/elegant-art.JPG" className="card-img-top" alt="screenshot of elegant-art"/>
-                        <div className="card-body">
-                          <h5 className="card-title">elegant-art</h5>
-                          <p className="card-text">Front-end project. This a website made for a relative. HTML, CSS, JavaScript, Bootstrap. Unfinished.</p>
-                          <a href="https://github.com/ericbwebdev86/elegant-art" className="btn btn-success me-2"><i class="fa-brands fa-github"></i>
-                            <p>Github Repo</p></a>
-                              <a href="http://elegantarte.com/" className="btn btn-success"><i class="fa-solid fa-sitemap"></i>
-                              <p>
-                              Deployed App
-                              </p></a>
-                        </div>
-                      </div>  
-                    </div>
-                    <div className="col">
-                        <div className="card mt-2 mb-2" >
                         <img src="./assets/images/code-quiz.JPG" className="card-img-top" alt="screenshot of code-quiz"/>
                         <div className="card-body">
                           <h5 className="card-title">code-quiz</h5>
-                          <p className="card-text">Front-end project. Uses HTML, CSS, and JavaScript, specifically dom manipulation and local storage.</p>
-                          <a href="https://github.com/ericbwebdev86/code-quiz" className="btn btn-success me-2"><i class="fa-brands fa-github"></i>
+                          <p className="card-text">Front-end project. Uses HTML, CSS, and JavaScript. This project focuses on dom manipulation and local storage.</p>
+                          <a href="https://github.com/ericbwebdev86/code-quiz" className="btn btn-success me-2 mb-2">
                             <p>Github Repo</p></a>
-                              <a href="https://ericbwebdev86.github.io/code-quiz/" className="btn btn-success"><i class="fa-solid fa-sitemap"></i>
+                              <a href="https://ericbwebdev86.github.io/code-quiz/" className="btn btn-success mb-2 me-2">
                               <p>
                               Deployed App
                               </p></a>
